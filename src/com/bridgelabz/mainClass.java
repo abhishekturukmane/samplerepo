95950d5823569f11339f6fd7ec7f9cfe92df9a99
abhishekturukmane <abhishekturukmaneofficial98@gmail.com>
2022-10-27 00:24:58 -0700

    Starting of project.

---
+++ src/com/bridgelabz/mainClass.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class mainClass {
+ /*public mainClass() {
+	// TODO Auto-generated constructor stub
+}*/
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+}
